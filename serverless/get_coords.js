a3854c5a59422cc198e8ccd5dd5c9e4e114f0e72
dill337 <64544519+dill337@users.noreply.github.com>
2021-07-13 15:17:07 -0500

    fixed serverless function code

--- serverless/get_coords.js
+++ serverless/get_coords.js
@@ -1,13 +1,13 @@
 const fetch = require("node-fetch");
 
-const { WEATHER_API_KEY} = process.env;
+const { WEATHER_API_KEY } = process.env;
 
 exports.handler = async (event, context) => {
   const params = JSON.parse(event.body);
   const { text, units } = params;
   const regex = /^\d+$/g;
   const flag = regex.test(text) ? "zip" : "q";
-  const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&units=${units}&appid=${WEATHER_API_KEY}`
+  const url = `https://api.openweathermap.org/data/2.5/weather?${flag}=${text}&units=${units}&appid=${WEATHER_API_KEY}`
   const encodedUrl = encodeURI(url);
   try {
     const dataStream = await fetch(encodedUrl);
@@ -17,6 +17,6 @@ exports.handler = async (event, context) => {
       body: JSON.stringify(jsonData)
     };
   } catch (err) {
-    return { statusCode: 422, body: err.stack }
+    return { statusCode: 422, body: err.stack };
   }
-} 
+};
